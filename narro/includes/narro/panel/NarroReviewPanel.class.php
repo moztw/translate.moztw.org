0b57d79ac95a016f4e42f261d17cd6509c5aa74a
Alexandru Szasz <alexandru.szasz@lingo24.com>
2012-12-02 17:53:41 +0200

    Fixes after upgrading qcubed.
    Signed-off-by: Peter Pin-Guang Chen <petercpg@mail.moztw.org>

--- narro/includes/narro/panel/NarroReviewPanel.class.php
+++ narro/includes/narro/panel/NarroReviewPanel.class.php
@@ -21,27 +21,26 @@
         public function __construct($objParentObject, $strControlId = null) {
             parent::__construct($objParentObject, $strControlId);
         }
-        
+
         public function dtrText_Create() {
             parent::dtrText_Create();
             $this->dtrText->Template = dirname(__FILE__) . '/NarroReviewPanel_DataRepeater.tpl.php';
         }
-        
+
         public function lstFilter_Create() {
             $this->lstFilter = new QListBox($this);
-            $this->lstFilter->RenderWithNameCssClass = 'inline_block';
             $this->lstFilter->Name = t('Show');
             $this->lstFilter->AddItem(t('unapproved texts'), self::SHOW_NOT_APPROVED, true);
             $this->lstFilter->AddItem(t('approved texts'), self::SHOW_APPROVED);
             $this->lstFilter->AddItem(t('translated or approved texts'), self::SHOW_APPROVED_AND_NOT_APPROVED);
             $this->lstFilter->AddItem(t('translated with no votes'), self::SHOW_NOT_APPROVED_AND_WITHOUT_VOTES);
             $this->lstFilter->AddItem(t('translated with votes'), self::SHOW_NOT_APPROVED_AND_WITH_VOTES);
-            
+
             $this->lstFilter->AddAction(new QChangeEvent(), new QAjaxControlAction($this, 'btnSearch_Click'));
             if (QApplication::QueryString('t'))
                 $this->lstFilter->SelectedValue = QApplication::QueryString('t');
         }
-        
+
         public function lstSort_Create() {
             $this->lstSort = new QListBox($this);
             $this->lstSort->RenderWithNameCssClass = 'inline_block';
@@ -51,10 +50,10 @@
             if (QApplication::QueryString('o'))
                 $this->lstSort->SelectedValue = QApplication::QueryString('o');
         }
-        
+
         public function chkRefresh_Create() {
             parent::chkRefresh_Create();
             $this->chkRefresh->Checked = true;
         }
-        
+
     }
