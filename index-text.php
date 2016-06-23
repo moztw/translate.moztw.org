b4a69c76ef4b1e1bced7b268834d3dd1086fbfd7
Peter Pin-Guang Chen <petercpg@mail.moztw.org>
2016-06-23 11:10:46 +0800

    MXR->DXR; httpsify

--- index-text.php
+++ index-text.php
@@ -6,14 +6,14 @@ header('Content-Type: text/plain; charset=utf-8');
 
 歡迎光臨翻譯站，此站（第三代）僅供團隊內部使用。
 
-請前往 http://translate.moztw.org/narro/
+請前往 https://translate.moztw.org/narro/
  - 若您是 l10n 團隊的一員或是已接受邀請
  - 想要看看尚未 push 到 Mozilla 的翻譯進度
 
 請前往 https://l10n.mozilla.org/teams/zh-TW 或 https://l10n-stage-sj.mozilla.org/dashboard/?locale=zh-TW
  - 若您想要知道目前各程式在 Mozilla repository 的狀態
 
-要瀏覽檔案請洽 http://mxr.mozilla.org/
+要瀏覽檔案請洽 https://dxr.mozilla.org/
 
 -----------
 
@@ -25,13 +25,13 @@ header('Content-Type: text/plain; charset=utf-8');
  英文版的來源是 /home/moztw/translate/var/repo，
  softlink 到各 project 的 /home/moztw/translate/var/narro/data/import/*/en-US 目錄，
  有 crontab 自動同步。
- 可以在 http://translate.moztw.org/update-log.txt 看到紀錄。
+ 可以在 https://translate.moztw.org/update-log.txt 看到紀錄。
 
  中文檔案 Export 之後可以下載 tar.gz，
  或是登入進機器裡 cp /home/moztw/translate/var/narro/data/import/*/zh-TW 的資料。
  
  ＊請注意，若未來 Mozilla 為新版本開啟了新的 repository，
-   （像是 http://hg.mozilla.org/releases/mozilla-1.9.1/）
+   （像是 https://hg.mozilla.org/releases/mozilla-1.9.1/）
    您有必要自行 checkout 新的 repository 並在 Narro 建立
    相對應 Project、import 目錄與 en-US softlink。
    不要把該 push 到不同 l10n repository 混在一起。
--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 <body>
 	<h1>MozTW 翻譯站</h1>
 	<blockquote>
-		<p><strong>English: </strong>This is where MozTW hosts its "bridging" services to localizing Mozilla products. Primary, there is a <a href="https://code.google.com/p/narro/">Narro</a> <a href="./narro/">installation</a> which separates files by products (with a project marked "shared" contains code for all Mozilla app). Some other scripts was also written to import and correct translations between repositories and Narro database. <strong>Any changes will and should always inspected by human before commit to the repository.</strong></p>
+		<p><strong>English: </strong>This is where MozTW hosts its "bridging" services to localizing Mozilla products. Primary, there is a <a href="https://github.com/alexxed/narro">Narro</a> <a href="./narro/">installation</a> which separates files by products (with a project marked "shared" contains code for all Mozilla app). Some other scripts was also written to import and correct translations between repositories and Narro database. <strong>Any changes will and should always inspected by human before commit to the repository.</strong></p>
 	</blockquote>
-	<p>歡迎光臨翻譯站，此站（第三代）僅供在地化團隊內部使用。這裡的翻譯建議並沒有被監控，想要回報錯誤請到討論區的<a href="http://forum.moztw.org/viewforum.php?f=4">在地化板回報</a>。有意長期貢獻請或接手翻譯專案的話請<a href="http://moztw.org/contribute/">加入我們</a>，到 mailing list 報到！</p>
+	<p>歡迎光臨翻譯站，此站（第三代）僅供在地化團隊內部使用。這裡的翻譯建議並沒有被監控，想要回報錯誤請到討論區的<a href="https://forum.moztw.org/viewforum.php?f=4">在地化板回報</a>。有意長期貢獻請或接手翻譯專案的話請<a href="https://moztw.org/contribute/">加入我們</a>，到 mailing list 報到！</p>
     <p>請前往 <a href="https://wiki.mozilla.org/L10n:Teams:zh-TW">Team Page at MozillaWiki</a></p>
 	<ul>
 		<li>取得翻譯團隊成員與專案的相關資訊</li>
@@ -24,7 +24,7 @@
 		<li>若您是 l10n 團隊的一員或是已接受邀請願意參與翻譯</li>
 		<li>想要看看尚未 push 到 Mozilla 的翻譯進度</li>
 	</ul>
-	<p>請前往 <a href="http://translate.moztw.org/l10n-base/">L10n Base</a></p>
+	<p>請前往 <a href="https://translate.moztw.org/l10n-base/">L10n Base</a></p>
 	<ul>
 		<li>若您想要取得本站的 Narro 所匯出的資料（By file），這些資料可能已經或尚未送出至 Mozilla repository</li>
 	</ul>
@@ -37,7 +37,7 @@
 		<li>若您想要知道目前加入的網頁專案與部分產品（例如 Firefox for iOS）</li>
 		<li>送出各維護中專案的字串翻譯建議</li>
 	</ul>
-  <p>請前往 <a href="http://l10n.mozilla-community.org/~pascalc/langchecker/?locale=zh-TW">Lang format file checker</a></p>
+  <p>請前往 <a href="https://l10n.mozilla-community.org/~pascalc/langchecker/?locale=zh-TW">Lang format file checker</a></p>
 	<ul>
 		<li>若您想要知道 Snippet (.Lang) 格式字串檔的翻譯狀況</li>
 	</ul>
@@ -47,16 +47,15 @@
   </ul>
 	<p>第三方工具</p>
 	<ul>
-		<li><a href="http://mxr.mozilla.org/">mxr.mozilla.org</a> - 瀏覽 Mozilla 程式碼（Mozilla）</li>
-		<li><a href="http://transvision.mozfr.org/">Transvision glossary</a> - 詞彙位置搜尋（法國 L10n 團隊）</li>
-		<li>English CopyNPaste Translations for <a href="http://indianoss.sourceforge.net/mozilla/CopyNPasteFinder.php?q=zh-TW">Firefox</a>、<a href="http://indianoss.sourceforge.net/mozilla/CopyNPasteFinder_Aurora.php?q=zh-TW">Firefox Aurora</a> - 中英相同詞彙列表（印度團隊）</li>
+		<li><a href="https://dxr.mozilla.org/">dxr.mozilla.org</a> - 瀏覽 Mozilla 程式碼（Mozilla）</li>
+		<li><a href="https://transvision.mozfr.org/">Transvision</a> - 詞彙位置搜尋（法國 L10n 團隊）</li>
 	</ul>
-	<p>所有非 <a href="http://blog.mozilla.com/blog/2011/04/13/new-channels-for-firefox-rapid-releases/">Rapid Release Cycle</a> 下的舊版本翻譯已不再維護並自翻譯站移除，若有需要請直接至 Mozilla 取回最終版本翻譯，或至 <a href="oldrepo/">此處</a> 取用備份（以專案與版本區分資料夾）。</p>
+	<p>所有非 <a href="https://blog.mozilla.org/blog/2011/04/13/new-channels-for-firefox-rapid-releases/">Rapid Release Cycle</a> 下的舊版本翻譯已不再維護並自翻譯站移除，若有需要請直接至 Mozilla 取回最終版本翻譯，或至 <a href="oldrepo/">此處</a> 取用備份（以專案與版本區分資料夾）。</p>
 	<hr />
 	<h2>著作權</h2>
-	<p>此網站上的所有翻譯皆為 <a href="http://www.mozilla.org/MPL/2.0/">MPL 2.0</a> 授權，與 Mozilla 所有程式碼的授權相同，供任何人在此授權條件下使用。<strong>如不同意該授權請勿在此站提供翻譯建議。</strong></p>
+	<p>此網站上的所有翻譯皆為 <a href="https://www.mozilla.org/MPL/2.0/">MPL 2.0</a> 授權，與 Mozilla 所有程式碼的授權相同，供任何人在此授權條件下使用。<strong>如不同意該授權請勿在此站提供翻譯建議。</strong></p>
 	<blockquote>
-		<p><strong>English:</strong> Anything on this site is licensed under <a href="http://www.mozilla.org/MPL/2.0/">MPL 2.0</a>, in line with Mozilla policy. Anyone is welcome to make use of these work under such condition. <strong>Do not contribute if you don't agree with the terms.</strong></p>
+		<p><strong>English:</strong> Anything on this site is licensed under <a href="https://www.mozilla.org/MPL/2.0/">MPL 2.0</a>, in line with Mozilla policy. Anyone is welcome to make use of these work under such condition. <strong>Do not contribute if you don't agree with the terms.</strong></p>
 	</blockquote>
 	<hr />
 	<h2>翻譯流程 (給 l10n owners)</h2>
@@ -74,7 +73,7 @@
 		<li>檢查所有修改是否正確：<code>hg diff</code> 或 <code>svn diff</code>，順便跑一下 compare-locales
 			<ul>
 				<li>刪掉 dashboard 說多餘的檔案</li>
-				<li>compare-locales: http://translate.moztw.org/compare-locales/?app=<code>{browser|mail|mobile|sync|calendar|suite|toolkit}</code>&amp;branch=<code>{central|aurora|beta}</code></li>
+				<li>compare-locales: https://translate.moztw.org/compare-locales/?app=<code>{browser|mail|mobile|sync|calendar|suite|toolkit}</code>&amp;branch=<code>{central|aurora|beta}</code></li>
 			</ul>
 		</li>
 		<li>確定沒有錯誤就 commit：<code>hg ci</code> 或 <code>svn ci</code>
@@ -87,7 +86,7 @@
 	<h2>準備工作目錄</h2>
 	<p>有工作目錄才能繼續 commit 東西加進去往上疊。要注意哪些程式碼該 commit 到哪去。</p>
 	<ol>
-		<li><code>hg clone http://hg.mozilla.org/releases/l10n-mozilla-1.9.2/zh-TW <strong>l10n-mozilla-1.9.2</strong></code></li>
+		<li><code>hg clone https://hg.mozilla.org/releases/l10n-mozilla-1.9.2/zh-TW <strong>l10n-mozilla-1.9.2</strong></code></li>
 	</ol>
 	<p>Windows 上面的作法請自行摸索 ...</p>
 	<h2>開新 Project 流程 (給 Narro maintainer)</h2>
@@ -95,7 +94,7 @@
 	在進入 Rapid Release Cycle 後 codebase 僅分為 <code>mozilla-central</code>、<code>mozilla-aurora</code>、<code>mozilla-beta</code>，所以這個情況可能不大容易再遇到...。</p>
 	<ol>
 		<li><code>cd /home/moztw/translate/var/repo;\<br>
-		sudo -u www-data hg clone http://hg.mozilla.org/releases/mozilla-1.9.2 <strong>mozilla-1.9.2</strong></code>
+		sudo -u www-data hg clone https://hg.mozilla.org/releases/mozilla-1.9.2 <strong>mozilla-1.9.2</strong></code>
 			<ul>
 				<li>repo 目錄底下的目錄都會被 crontab 自動更新</li>
 			</ul>
